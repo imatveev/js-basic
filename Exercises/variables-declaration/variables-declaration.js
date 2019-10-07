3f84d6dd8b7363bba681a9b1be68ffc91d520661
Mykyta Lakhnik <myla@ciklum.com>
2019-10-07 18:02:54 +0300

    prepared structure

---
+++ Exercises/variables-declaration/variables-declaration.js
@@ -0,0 +1,15 @@
+// Declaration
+// variable which will be encapsulated from global scope:
+
+
+
+// variable which will appear in global object:
+
+
+
+// two variables which will be same-names but contain different values:
+
+
+
+// Hoisting
+// variable with hosting:
