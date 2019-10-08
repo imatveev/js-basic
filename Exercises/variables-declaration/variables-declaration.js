efa3e181d741051f5c54647b5a4b55c771aa7719
Mykyta Lakhnik <myla@ciklum.com>
2019-10-08 09:18:18 +0300

    Home work 1: Variables declaration

--- Exercises/variables-declaration/variables-declaration.js
+++ Exercises/variables-declaration/variables-declaration.js
@@ -1,17 +1,31 @@
-// Declaration
-// variable which will be encapsulated from global scope:
+// # Declaration
+// 1. Declare a variable which will be encapsulated from global scope.
 
+if (true) {
+  let encapsulatedVariable = 777
+}
 
+// 2. Declare a variable which will appear in global object.
 
-// variable which will appear in global object:
+window.globalVariable = {
+  name: "John"
+};
 
+alert(window.globalVariable);
 
+// 3. Declare two variables which will be same-names but contain different values.
 
-// two variables which will be same-names but contain different values:
+let sameName = "Hello!";
 
-let message = "Hello!";
-let message = "World!"; //overrides the previous value
+if(true){
+  let sameName = "World!";
+}
 
+console.log(sameName);
 
-// Hoisting
-// variable with hosting:
+// # Hoisting
+// 1. Declare a variable with hosting.
+
+console.log(hostedVariable); // Output: undefined
+
+var hostedVariable = 'The variable with hosting.'
