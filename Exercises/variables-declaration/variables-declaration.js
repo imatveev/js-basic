27307bbd1b904ca7b9dc66b1bec04e880c4ab0b5
Andrey Mamchur <anmam@anmams-MacBook-Pro.local>
2019-10-08 11:22:42 +0300

    Done excersice

---
+++ Exercises/variables-declaration/variables-declaration.js
@@ -0,0 +1,33 @@
+//Declare a variable which will be encapsulated from global scope.
+
+function f() {
+    var a = 4;
+}
+
+
+if (true){
+    let x = 5;
+    const y = 7;
+}
+
+
+
+//Declare a variable which will appear in global object.
+
+var b = 3;
+let c = 4;
+const d = 5;
+
+
+
+//Declare two variables which will be same-names but contain different values.
+
+let X = 'c';
+let x = 3;
+
+
+//Declare a variable with hosting.
+
+
+console.log(a);
+var a = 3;
